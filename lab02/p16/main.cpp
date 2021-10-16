71c4c7796e0df039191ffcf2427d25c27bdec9f6
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-10-16 22:41:15 +0200

    lab02 p16 Accepted

--- lab02/p16/main.cpp
+++ lab02/p16/main.cpp
@@ -10,7 +10,7 @@ int main()
     iostream::sync_with_stdio(false);
 
     vector<string> song = {"Happy", "birthday", "to", "you", "Happy", "birthday", "to", "you",
-                           "Happy", "birthday", "to", "Rujia", "Happy", "birthday", "to", "you!"};
+                           "Happy", "birthday", "to", "Rujia", "Happy", "birthday", "to", "you"};
 
     
     int n;
@@ -25,28 +25,25 @@ int main()
     int i = 0;
     int j = 0;
     bool isComplete = false;
+
     while (true)
     {
         cout << names[i] << ": " << song[j] << "\n";
-        if (i == n - 1)
-        {
+        if (i == n - 1) {
             isComplete = true;
         }
 
-        if (j == sz(song) - 1 && isComplete)
-        {
+        if (j == sz(song) - 1 && isComplete) {
             break;
         }
 
         ++i;
         ++j;
 
-        if (i == n)
-        {
+        if (i == n) {
             i = 0;
         }
-        if (j == sz(song))
-        {
+        if (j == sz(song)) {
             j = 0;
         }
     }
