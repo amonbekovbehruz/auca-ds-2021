2c85b81c3fb926996f74cbb11e733f66fb34e8fb
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-10-26 19:01:03 +0200

    completed the task until std::sort algorithm with std::vector

---
+++ lab01/p06/main.cpp
@@ -0,0 +1,173 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../doctest/doctest.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+TEST_CASE("vector's default constructor") 
+{
+    vector<int> v;
+
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 0);
+    REQUIRE(v.empty());
+}
+
+TEST_CASE("vector's move constructor") 
+{
+    vector<int> v1 = {1, 2, 3, 4, 5};
+
+    REQUIRE(v1.size() == 5);
+    
+    vector<int> v2 = move(v1);
+    REQUIRE(v2.size() == 5);
+    REQUIRE(v1.size() == 0);
+    REQUIRE(v1.empty());
+}   
+
+TEST_CASE("vector's constructor with count copies") 
+{
+    vector<int> v(5);
+
+    REQUIRE(v[0] == 0);
+    REQUIRE(v[1] == 0);
+    REQUIRE(v[2] == 0);
+    REQUIRE(v[3] == 0);
+    REQUIRE(v[4] == 0);
+
+    vector<string> v2(3);
+
+    REQUIRE(v2[0] == "");
+    REQUIRE(v2[1] == "");
+    REQUIRE(v2[2] == "");
+
+    vector<int> v3(3, 42);
+
+    REQUIRE(v3[0] == 42);
+    REQUIRE(v3[1] == 42);
+    REQUIRE(v3[2] == 42); 
+}
+
+TEST_CASE("vector's copy constructor")
+{
+    vector<int> v(3);
+    v[0] = 1;
+    v[1] = 2;
+    v[2] = 3;
+
+    vector<int> v2 = v;
+    v[1] = 1000;
+
+    REQUIRE(v2[0] != 1000);
+    REQUIRE(v2[1] == 2);
+} 
+
+TEST_CASE("vector's assignment constructor")
+{
+    vector<int> v = {1, 2, 3};
+    vector<int> v2 = v;
+
+    v[0] = 10;
+    v[1] = 20;
+
+    REQUIRE(v2[0] == 1);
+    REQUIRE(v2[1] == 2);
+}
+
+TEST_CASE("vector's subscript operator")
+{
+    vector<int> v(5);
+
+    v[0] = 1;
+    v[1] = 2;
+    v[2] = 3;
+
+    REQUIRE(v[0] == 1);
+    REQUIRE(v[1] == 2);
+    REQUIRE(v[2] == 3);
+}
+
+TEST_CASE("vector's method at(index) and it's exception") 
+{
+    vector<int> v(5);
+
+    v[0] = 1;
+    v[1] = 2;
+    v[2] = 3;
+
+    try
+    {
+        v.at(3) = 4;    
+    }
+    catch(out_of_range& e)
+    {
+        cout << "out of bounds" << '\n';
+    }
+    
+    REQUIRE(v[0] == 1);
+    REQUIRE(v[1] == 2);
+    REQUIRE(v[2] == 3);
+    REQUIRE(v[3] == 4);
+
+    try
+    {
+        v.at(5) = 4;    
+    }
+    catch(out_of_range& e)
+    {
+        cout << "out of bounds" << '\n';
+    }
+} 
+
+TEST_CASE("vector's push_back method") 
+{
+    vector<int> v;
+
+    v.push_back(20);
+    v.push_back(30);
+
+    REQUIRE(v.size() == 2);
+    REQUIRE(v.capacity() == 2);    
+    REQUIRE(v[0] == 20);
+    REQUIRE(v[1] == 30);
+}
+
+TEST_CASE("the work of for (auto element : container) statement with std::vector") 
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    for(auto &e: v) {
+        e++;
+    }
+
+    REQUIRE(v[0] == 2);    
+    REQUIRE(v[1] == 3);
+    REQUIRE(v[2] == 4);
+    REQUIRE(v[3] == 5);
+    REQUIRE(v[4] == 6);
+}
+
+TEST_CASE("the work of std::reverse algorithm with std::vector") 
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    reverse(v.begin(), v.end());
+
+    REQUIRE(v[0] == 5);    
+    REQUIRE(v[1] == 4);
+    REQUIRE(v[2] == 3);
+    REQUIRE(v[3] == 2);
+    REQUIRE(v[4] == 1);
+
+    reverse(v.begin(), v.end());
+
+    REQUIRE(v[0] == 1);    
+    REQUIRE(v[1] == 2);
+    REQUIRE(v[2] == 3);
+    REQUIRE(v[3] == 4);
+    REQUIRE(v[4] == 5);
+}
+
+TEST_CASE("the work of std::sort algorithm with std::vector")
