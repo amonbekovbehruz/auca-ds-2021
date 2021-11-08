8a1ab67e67b6b9c7cc311165b20bf9e44da7b9f9
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-11-08 19:27:26 +0100

    finsihed lab03 p33 wrong answer

---
+++ lab02/p33/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int testCases;
+    cin >> testCases;
+
+    for (int i = 0; i < testCases; i++)
+    {
+        int numbersOfStudents;
+        cin >> numbersOfStudents;
+        int sum = 0;
+        vector<int> percentage(numbersOfStudents, 0);
+
+        for (int j = 0; j < numbersOfStudents; ++j)
+        {
+            int result;
+            cin >> result;
+            sum += result;
+            percentage[j] = result;
+        }
+
+        double average = sum * 1.0 / numbersOfStudents;
+        int aboveAverage = 0;
+
+        for (int j = 0; j < numbersOfStudents; j++)
+        {
+            if (percentage[j] > average)
+            {
+                aboveAverage++;
+            }
+        }
+
+        double aboveAverageInPercents = (aboveAverage * 10000.0 / numbersOfStudents);
+
+        cout << fixed << setprecision(3) << aboveAverageInPercents << "%" << '\n';
+    }
+}
