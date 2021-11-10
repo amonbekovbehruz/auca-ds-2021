67be36a613c62b87b792683bf0166605a793fb51
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-11-10 11:44:31 +0100

    finished lab02 p19 accepted

--- lab02/p19/main.cpp
+++ lab02/p19/main.cpp
@@ -1,14 +1,9 @@
 #include <bits/stdc++.h>
 
-template <typename C>
-int sz(const C &c) { return static_cast<int>(c.size()); }
-
 using namespace std;
 
 int main()
 {
-    iostream::sync_with_stdio(false);
-
     string line;
     getline(cin, line);
 
