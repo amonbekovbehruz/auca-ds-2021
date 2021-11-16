cb4f03b11645b5698d5a7b45eaf1735bd9a50636
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-11-16 19:14:50 +0100

    finished lab02 p24 wrong answer

---
+++ lab02/p24/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> numbers(n);
+        for (auto &e : numbers)
+        {
+            cin >> e;
+        }
+
+        sort(begin(numbers), end(numbers));
+
+        int mid = numbers[n];
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            ans += abs(numbers[i] - mid);
+        }
+        cout << ans << "\n";
+    }
+}
