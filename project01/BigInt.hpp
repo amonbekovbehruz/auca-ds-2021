e509aaba0dee7aa024f41f0cf39598c6a1298fe8
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-12-10 12:49:28 +0100

    worked on operator+. Wrong answer

--- project01/BigInt.hpp
+++ project01/BigInt.hpp
@@ -83,7 +83,7 @@ std::ostream &operator<<(std::ostream &sout, const BigInt &r)
     return sout;
 }
 
-BigInt operator+(const BigInt &first, const BigInt &second)
+BigInt operator+(BigInt &first, BigInt &second)
 {
     std::vector<int> firstVector = first.getVector();
     std::vector<int> secondVector = second.getVector();
@@ -93,32 +93,23 @@ BigInt operator+(const BigInt &first, const BigInt &second)
     std::reverse(firstVector.begin(), firstVector.end());
     std::reverse(secondVector.begin(), secondVector.end());
 
-    std::vector<int> result = firstVector;
+    std::vector<int> result = firstVector.size() > secondVector.size() ? firstVector : secondVector;
+
     bool toAddDigit = false;
     for (int i = 0; i < (int)minSize; i++)
     {
-        if (firstVector[i] + secondVector[i] > 9)
+        if (toAddDigit)
         {
-            result[i] = firstVector[i] + secondVector[i] % 10;
-            if (toAddDigit)
-            {
-                result[i] += 1;
-            }
+            result[i]++;
+        }
+        if ((firstVector[i] + secondVector[i]) >= 10)
+        {
             toAddDigit = true;
+            result[i] = (firstVector[i] + secondVector[i]) % 10;
         }
         else
         {
             result[i] = firstVector[i] + secondVector[i];
-            if (toAddDigit)
-            {
-                result[i] += 1;
-                toAddDigit = false;
-            }
-            if (result[i] > 9)
-            {
-                result[i] %= 10;
-                toAddDigit = true;
-            }
         }
     }
 
