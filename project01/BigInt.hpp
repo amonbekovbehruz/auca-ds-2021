55dc537564fc48956ec31696285f40b956c9d24a
Amonbekov Behruz <shosamonbekov_b@auca.kg>
2021-12-27 18:12:18 +0100

    created constructor with

--- project01/BigInt.hpp
+++ project01/BigInt.hpp
@@ -1,171 +1,64 @@
-#pragma once
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <stdexcept>
 
 class BigInt
 {
-    friend std::ostream &operator<<(std::ostream &out, const BigInt &x);
-    friend BigInt operator+(const BigInt &a, const BigInt &b);
-    void applyToVector(const std::string &str, std::vector<int> &vector)
-    {
-        for (char num : str)
-        {
-            if (!(num >= 48 && num <= 57))
-            {
-                throw std::runtime_error("BigInt: invalid input");
-            }
-
-            vector.push_back((int)num - 48);
-        }
-    }
-
-    static int compare(const BigInt &a, const BigInt &b)
-    {
-        if (a.mDigits.size() > b.mDigits.size())
-        {
-            return 1;
-        }
-        if (a.mDigits.size() < b.mDigits.size())
-        {
-            return -1;
-        }
-        for (int i = 0; i < a.mDigits.size(); ++i)
-        {
-            if (a.mDigits[i] != b.mDigits[i])
-            {
-                return (a.mDigits[i] - b.mDigits[i]);
-            }
-        }
-
-        return 0;
-    }
-
-    static BigInt addAbsValues(const BigInt &first, const BigInt &second)
-    {
-        BigInt result;
-        result.mDigits.clear();
-
-        auto i = first.mDigits.rbegin();
-        auto j = second.mDigits.rbegin();
-
-        int carry = 0;
-        while (i != first.rend() || j != second.rend())
-        {
-            int sum = carry;
-
-            if (i != first.mDigits.rend())
-            {
-                sum += *i;
-                i++;
-            }
-            if (j != second.mDigits.rend())
-            {
-                sum += *j;
-                j++;
-            }
-            result.mDigits.push_back(sum % 10);
-            carry = sum / 10;
-        }
-    }
-
-    static BigInt subAbsValues(const BigInt &first, const BigInt &second)
-    {
-    }
-
-    bool isNegative = false;
     std::vector<int> mDigits;
+    bool mIsNegative;
 
 public:
     BigInt()
+        : mIsNegative(false)
     {
-        mDigits = std::vector<int>(1, 0);
-        isNegative = false;
-    };
 
-    BigInt(const std::string &nums)
-    {
-        nums.at(0) == '-' ? isNegative = true : isNegative = false;
-        applyToVector(nums, mDigits);
+        mDigits.push_back(0);
     }
 
-    BigInt(const std::vector<int> &res)
+    BigInt(const std::string &value)
+        : mIsNegative(false)
     {
-        mDigits = res;
-    }
-
-    BigInt(const long long &num)
-    {
-        if (num < 0)
+        if (value.empty())
         {
-            isNegative = true;
+            throw std::runtime_error("BigInt: empty string");
         }
-        std::string str = std::to_string(num);
+        size_t i = 0;
+        if (value[i] == '-' || value[i] == '+')
+        {
+            mIsNegative = value[i] == '-';
+            ++i;
+        }
 
-        applyToVector(str, mDigits);
+        while (i + 1 != value.size() && value[i] == '0')
+        {
+            ++i;
+        }
+
+        for (; i != value.size(); ++i)
+        {
+            if (!isdigit(value[i]))
+            {
+                throw std::runtime_error("BigInt: Incorrect string value");
+            }
+
+            mDigits.push_back(value[i] - '0');
+        }
+
+        if (mDigits.empty())
+        {
+            throw std::runtime_error("BigInt: Incorrect string value");
+        }
+    }
+
+    BigInt(long long x)
+
+    {
     }
 
     const std::vector<int> getVector() const
     {
         return mDigits;
     }
-
-    const int size() const
+    std::vector<int> getVector()
     {
-        return (int)mDigits.size();
+        return mDigits;
     }
-
-    const bool checkForNegativity() const
-    {
-        return isNegative;
-    }
-};
-
-std::ostream &operator<<(std::ostream &sout, const BigInt &r)
-{
-    if (r.checkForNegativity())
-    {
-        sout << '-';
-    }
-    for (auto c : r.getVector())
-    {
-        sout << c;
-    }
-
-    return sout;
-}
-
-bool operator==(const BigInt &first, const BigInt &second)
-{
-    if (first.checkForNegativity == second.checkForNegativity && first.getVector == second.getVector)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-bool operator!=(const BigInt &first, const BigINt &second)
-{
-    return !(first == second);
-}
-
-inline BigInt operator+(const BigInt &first, const BigInt &second)
-{
-    if (first.isNegative == second.isNegative)
-    {
-        BigInt result;
-        result = BigInt::addAbsValues(first, second);
-        return result;
-    }
-    int compare = BigInt::compare(first, second);
-}
-
-inline BigInt operator-(const BigInt &first, const BigInt &second)
-{
 }
